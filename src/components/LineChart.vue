6ab154830b9da413c4bf34b49d528bf0354867a4
seijihosokawa <68927618+seijihosokawa@users.noreply.github.com>
2021-06-05 23:14:32 -0400

    formatting labels

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,5 +1,4 @@
 <script>
-import { Object } from "core-js";
 import { Line } from "vue3-chart-v2";
 
 export default {
@@ -17,15 +16,15 @@ export default {
     async getApiDataHandler() {
       var data = await this.getApiData();
       console.log(data);
-      this.timeStamps = data.timestamp;
-      this.closingPrices = data.indicators.quote[0].close;
-
-      console.log(this.timeStamps);
-      console.log(this.closingPrices);
+      return data;
     },
     getApiData() {
       var interval = "15m";
+      if (this.dateRange === "5d") interval = "1d";
+      if (this.dateRange === "1m") interval = "1w";
+      if (this.dateRange === "3m" || this.dateRange === "6m") interval = "1m";
       var dateRange = this.dateRange;
+      //other token: c4eac4392cmsh76076d1e061f713p1b7aa9jsn6f47c253ffd9
       console.log("getting api data");
       return new Promise(function (resolve) {
         fetch(
@@ -34,7 +33,7 @@ export default {
             method: "GET",
             headers: {
               "x-rapidapi-key":
-                "c4eac4392cmsh76076d1e061f713p1b7aa9jsn6f47c253ffd9",
+                "b44712c8bfmshfd507b16c8e731bp1c8a77jsned08f4602d3d",
               "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
             },
           }
@@ -49,40 +48,45 @@ export default {
       });
     },
     renderChartData() {
-      console.log(this.timeStamps);
-      console.log(this.closingPrices);
+      this.getApiDataHandler().then((data) => {
+        this.timeStamps = data.timestamp;
+        this.closingPrices = data.indicators.quote[0].close;
+        console.log(this.timeStamps);
+        console.log(this.closingPrices);
+        this.timeStamps.forEach(function (val, index, arr) {
+          arr[index] = new Date(val * 1000).toLocaleString().split(",")[1];
+        });
+        console.log(this.timeStamps);
 
-      var dataCollection = {
-        labels: [1, 2, 3, 4, 5, 6, 7],
-        datasets: [
-          {
-            label: "My First Dataset",
-            data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: "rgb(75, 192, 192)",
-            tension: 0.1,
-          },
-        ],
-      };
-      var options = {
-        responsive: true,
-        title: {
-          display: true,
-          text: "Portfolio vs S&P 500",
-          fontColor: "white",
-        },
-        legend: {
-          display: true,
-          labels: {
+        var dataCollection = {
+          labels: this.timeStamps,
+          datasets: [
+            {
+              label: "My First Dataset",
+              data: this.closingPrices,
+              fill: false,
+              borderColor: "rgb(75, 192, 192)",
+              tension: 0.1,
+            },
+          ],
+        };
+        var options = {
+          responsive: true,
+          title: {
+            display: true,
+            text: "Portfolio vs S&P 500",
             fontColor: "white",
           },
-        },
-      };
-      this.renderChart(dataCollection, options);
+          legend: {
+            display: true,
+            labels: {
+              fontColor: "white",
+            },
+          },
+        };
+        this.renderChart(dataCollection, options);
+      });
     },
-  },
-  created() {
-    this.getApiDataHandler();
   },
   mounted() {
     this.renderChartData();
